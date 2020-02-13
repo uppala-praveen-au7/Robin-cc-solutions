02dace00d10e4a029f56fe1765d3f0e5c051ecf2
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2020-02-13 21:36:32 +0530

    Updated CC of week03 day04

--- week03/day04/leapYear.py
+++ week03/day04/leapYear.py
@@ -1,9 +1,7 @@
 def is_leap(year):
     leap = False
 
-    #this condition checks for the leap year ex:- 2004, 2008
     if year%4 == 0:
-        #these two condiiton checks for the century leap year ex:- 2000
         if year%100 == 0:
             if year%400 == 0:
                 leap = True
@@ -18,6 +16,3 @@ def is_leap(year):
 
 year = int(input())
 print(is_leap(year))
-
-#check more about leap year
-#https://www.youtube.com/watch?v=56zlm9qhVGc
